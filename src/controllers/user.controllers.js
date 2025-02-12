7340505dbe371392f83171e9a9984556cfc96dee
r4478 <rk21eeb0a57@student.nitw.ac.in>
2025-02-12 22:14:16 +0530

    correction

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -33,7 +33,7 @@ const registerUser = asyncHandler(async(req,res)=>{
     }
 
 
-    const existedUser = User.findOne({
+    const existedUser =await User.findOne({
         $or: [{ username },{ email }]
     })
     if(existedUser){
